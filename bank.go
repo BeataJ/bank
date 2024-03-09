d7870db16ca103c773cded7ef67022ddfbc1331a
Beata <beata@jasinski.com>
2024-03-09 14:47:55 -0800

    break, continue

--- bank.go
+++ bank.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	var accountBalance = 1000.0
 
-	for i := 0; i < 2; i++ {
+	fmt.Println("Welcome to Go Bank!")
+	for {
 
-		fmt.Println("Welcome to Go Bank!")
 		fmt.Println("What do you want to do?")
 		fmt.Println("1. Check balance")
 		fmt.Println("2. Deposit money")
@@ -29,7 +29,8 @@ func main() {
 
 			if depositAmount <= 0 {
 				fmt.Println("Invalid amount. Must be greater than 0.")
-				return
+				// return
+				continue
 			}
 
 			accountBalance += depositAmount
@@ -53,6 +54,10 @@ func main() {
 			fmt.Println("Balance updated! New amount:", accountBalance)
 		} else {
 			fmt.Println("Goodbye!")
+			// return
+			break
 		}
 	}
+
+	fmt.Println("Thanks for choosing our bank")
 }
